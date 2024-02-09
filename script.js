59b9493daa8ca3a91d38bc6eab95f858b24e38b3
FelipeAugustoFialho <felipefialho.informatica@gmail.com>
2024-02-08 22:43:40 -0300

    Adicionando Design Responsivo

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ function generateNumber() {
    const max = Math.floor(document.querySelector('.input-max').value)
    
    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-   
-   console.log(min,max)
+       
    alert (result)
 }
 
